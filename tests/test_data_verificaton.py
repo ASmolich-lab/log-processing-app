20ff8504dc7e9ca8b8a5351473589aef7e1e80c6
Alex S <alexandr.smolich@gmail.com>
2026-01-14 14:17:00 -0500

    Publishing test results. Added test.

--- tests/test_data_verificaton.py
+++ tests/test_data_verificaton.py
@@ -1,18 +1,23 @@
 import subprocess
 
-def test_logs_are_not_empty():
-    # Verify target_1 receives data from the Agent/Splitter.
-    
+def test_log_line_count():
+    # Verify target_1 receives data from the Agent/Splitter. Checking # of lines
     result = subprocess.run(
-        ["docker", "exec", "target_1", "cat", "events.log"],
-        capture_output=True, 
-        text=True, 
-        check=True
+        ["docker", "exec", "target_1", "sh", "-c", "wc -l < events.log"],
+        capture_output=True, text=True, check=True
     )
     
-    logs = result.stdout.strip()
+    line_count = int(result.stdout.strip())
     
-    # 3. Assertion: If the string length is 0, the test fails
-    assert len(logs) > 0, "Target events.log is empty!"
+    assert line_count > 0, "Target events.log has no data!"
     
-    print(f"target_1/events.log contains {len(logs)} chars of data.")
+    print(f"target_1/events.log contains {line_count} lines.")
+
+def test_targets_output_comparison():
+    # Verify target_1 and target_1 has same anount of lines in output
+    count1 = subprocess.run(["docker", "exec", "target_1", "sh", "-c", "wc -l < events.log"],
+                            capture_output=True, text=True).stdout.strip()
+    count2 = subprocess.run(["docker", "exec", "target_2", "sh", "-c", "wc -l < events.log"],
+                            capture_output=True, text=True).stdout.strip()
+    
+    assert count1 == count2, f"Sync Error: Target1({count1}) != Target2({count2})"
